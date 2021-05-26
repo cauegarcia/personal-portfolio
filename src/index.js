410a39970051a8238c8e9cc016551febc5fd997c
Caue Moyano <cauegmoyano@gmail.com>
2021-05-25 21:20:24 -0300

    fixed applyOnScroll function and initialized contact section

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ const setUpSettings = () => {
   const applyPositionOnSection = (sectionToApply) => {
     newYoffset = window.pageYOffset;
     const section = document.getElementById(sectionToApply);
-    const sectionNameHeight = section.firstElementChild.offsetHeight;
-    const lastChild = section.lastElementChild.lastElementChild;
-
     const intViewportHeight = window.innerHeight;
     const intViewportWidth = window.innerWidth;
 
+    if (section.offsetHeight < intViewportHeight * 1.1) return;
+    const sectionNameHeight = section.firstElementChild.offsetHeight;
     sectionTop = section.getBoundingClientRect().top;
     sectionBottom = section.getBoundingClientRect().bottom;
     sectionNameRight = section.getBoundingClientRect().right;
+    sectionFirstChildTop =
+      section.lastElementChild.firstElementChild.getBoundingClientRect().top;
     sectionLastChildTop =
       section.lastElementChild.lastElementChild.getBoundingClientRect().top;
     sectionLastChildBottom =
@@ -45,10 +46,12 @@ const setUpSettings = () => {
           section.lastElementChild.firstElementChild.style.marginTop = `${sectionNameHeight}px`;
         }
         //unfix name div
-        if (sectionLastChildTop < navHeight + sectionNameHeight) {
+        if (sectionBottom < intViewportHeight) {
           section.firstElementChild.classList.remove("fixed-name");
           section.firstElementChild.classList.add("absolute-name");
-          section.firstElementChild.style.marginBottom = `${lastChild.offsetHeight}px`;
+          section.firstElementChild.style.marginBottom = `${
+            intViewportHeight - navHeight - sectionNameHeight
+          }px`;
         }
       } else {
         //desktop config
@@ -60,22 +63,15 @@ const setUpSettings = () => {
     } else {
       //scrolling up
       yoffset = newYoffset;
-      //both devices
-      if (sectionTop > navHeight) {
-        section.firstElementChild.classList.remove("fixed-name");
-      }
-
       //phones and tablets config
       if (intViewportWidth < 1024) {
         //unfix name div
-        if (sectionTop > navHeight) {
+        if (sectionFirstChildTop > navHeight + sectionNameHeight) {
+          section.firstElementChild.classList.remove("fixed-name");
           section.lastElementChild.firstElementChild.style.marginTop = `0px`;
         }
         //fix name div
-        if (
-          sectionTop < 0 &&
-          sectionLastChildTop > navHeight + sectionNameHeight
-        ) {
+        if (sectionTop < 0 && sectionBottom > intViewportHeight) {
           section.firstElementChild.classList.remove("absolute-name");
           section.firstElementChild.classList.add("fixed-name");
           section.firstElementChild.style.marginBottom = "0";
@@ -83,6 +79,10 @@ const setUpSettings = () => {
       } else {
         //desktop config
         //unfix name div
+        if (sectionFirstChildTop > navHeight) {
+          section.firstElementChild.classList.remove("fixed-name");
+        }
+        //fix name div
         if (sectionTop < 0 && sectionLastChildBottom > innerHeight) {
           section.firstElementChild.classList.add("fixed-name");
         }
